1648c44364d4dc83a5a09aeb3aa2652fbf813966
shaimaa <shaimafikry@yahoo.com>
2023-12-01 23:30:03 +0200

    edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+for i in range(9):
+    for x in range(i+1,10):
+        if x != i:
+            if i == 8 and x == 9:
+                print("{}{}".format(i,x))
+                break
+            print("{}{}".format(i,x), end=', ')
