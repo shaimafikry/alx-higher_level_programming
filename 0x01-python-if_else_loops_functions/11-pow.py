f0d3509d5523a6324e510dbfee308b2b8626b66a
shaimaa <shaimafikry@yahoo.com>
2023-12-03 00:50:31 +0200

    edit

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+def pow(a, b):
+    res = 1
+    if b == 0:
+        return 1
+    elif b < 0:
+        b = b * -1
+        while (b != 0):
+             print (b)
+             res = res * 1/a
+             b = b - 1
+    else:
+        while (b != 0):
+            print (b)
+            res = res * a
+            b = b - 1
+    return res
+
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(1000, -2))
+print(pow(4, 5))
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+
+    if number >= 0:
+        m = number % 10
+    else:
+        m = number % -10
+        m = m * -1
+    print(m, end="")
+    return m
