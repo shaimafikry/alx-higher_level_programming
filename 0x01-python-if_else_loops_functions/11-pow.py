445fca0e6c90e53b8e786b80a6d51fe3f1b89eeb
shaimaa <shaimafikry@yahoo.com>
2023-12-03 12:55:51 +0200

    edit

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    res = 1
-    if b == 0:
-        return 1
-    elif b < 0:
-        b = b * -1
-        while b != 0:
-            res = res * 1 / a
-            b = b - 1
-    else:
-        while b != 0:
-            res = res * a
-            b = b - 1
-    return res
+    return a ** b
