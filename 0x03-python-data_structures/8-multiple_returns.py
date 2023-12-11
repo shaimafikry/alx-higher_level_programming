9c137e0e31e0600d84165a1839f38ddc5c987c39
shaimaa <shaimafikry@yahoo.com>
2023-12-11 09:46:57 +0200

    edit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if len(sentence):
         return (len(sentence), sentence[0])
     else:
-            return (len(sentence), None)
+        return (len(sentence), None)
