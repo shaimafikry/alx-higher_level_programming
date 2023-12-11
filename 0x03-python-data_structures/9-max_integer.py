b6821747560c97b054bb11fcdd55110861464ecc
shaimaa <shaimafikry@yahoo.com>
2023-12-11 10:11:37 +0200

    edit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not len(my_list):
+        return None
+    big = 0
+    for i in my_list:
+        if i > big:
+            big = i
+    return big
