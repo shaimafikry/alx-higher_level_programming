27e72371ed2610932c9373729131755ce7e4a4ef
shaimaa <shaimafikry@yahoo.com>
2023-12-11 12:12:39 +0200

    edit

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -9,7 +9,7 @@ def add(a, b):
     Returns:
         The return value. a + b
     """
-    return (a + b)
+    return a + b
 
 
 def sub(a, b):
@@ -22,7 +22,7 @@ def sub(a, b):
     Returns:
         The return value. a - b
     """
-    return (a - b)
+    return a - b
 
 
 def mul(a, b):
@@ -35,7 +35,7 @@ def mul(a, b):
     Returns:
         The return value. a * b
     """
-    return (a * b)
+    return a * b
 
 
 def div(a, b):
