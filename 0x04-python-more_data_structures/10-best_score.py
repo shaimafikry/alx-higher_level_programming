4501d5322dcc528725b498871658bb6bb5149005
shaimaa <shaimafikry@yahoo.com>
2023-12-15 22:53:13 +0200

    edit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Write a function that returns a key with the biggest integer value.(a_dictionary):
-# You can assume that all values are only integers
+# You can assume all values only integers
 # If no score found, return None
 # You can assume all students have a different score
 # You are not allowed to import any module
