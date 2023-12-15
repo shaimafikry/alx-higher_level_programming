90a73134519a2d3ef887bdb334cf55730002a78c
shaimaa <shaimafikry@yahoo.com>
2023-12-15 22:43:06 +0200

    edit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Write a function that returns a key with the biggest integer value.(a_dictionary):
+# You can assume that all values are only integers
+# If no score found, return None
+# You can assume all students have a different score
+# You are not allowed to import any module
+def best_score(a_dictionary):
+    if a_dictionary:
+        r = 0
+        for k, m in a_dictionary.items():
+            if m > r:
+                r = m
+        for k, m in a_dictionary.items():
+            if r == m:
+                return k
+    else:
+        return None
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Write a function that returns a new dictionary with all values multiplied by 2
+# Prototype: def multiply_by_2(a_dictionary):
+# You can assume that all values are only integers
+# Returns a new dictionary
+# You are not allowed to import any module
+def multiply_by_2(a_dictionary):
+    cp = a_dictionary.copy()
+    for i, m in cp.items():
+        cp[i] = m * 2
+    return cp
