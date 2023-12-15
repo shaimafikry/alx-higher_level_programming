206327956b20be0eb099db80f412c8d36dd21ce0
shaimaa <shaimafikry@yahoo.com>
2023-12-15 04:40:33 +0200

    edit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Write a function that adds all unique integers in a list (only once for each integer).
+# Write a function that adds all unique integers in a list
+# (only once for each integer).
 # Prototype: def uniq_add(my_list=[]):
 # You are not allowed to import any module
 def uniq_add(my_list=[]):
