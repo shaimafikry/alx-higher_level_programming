6aa4dfd2067efc9973e8bb542630fbb4f5e3a991
shaimaa <shaimafikry@yahoo.com>
2023-12-15 22:48:58 +0200

    edit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Write a function that returns a new dictionary with all values multiplied by 2
+# function that returns a new dictionary with all values multiplied by 2
 # Prototype: def multiply_by_2(a_dictionary):
 # You can assume that all values are only integers
 # Returns a new dictionary
