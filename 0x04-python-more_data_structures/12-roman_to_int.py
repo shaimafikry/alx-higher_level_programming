463699af3bfe1b25ac05841cf4bd0d35ce667694
shaimaa <shaimafikry@yahoo.com>
2023-12-16 01:38:06 +0200

    edit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@
 # def roman_to_int(roman_string) must return an integer
 # If the roman_string is not a string or None, return 0
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
     r_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     num = 0
@@ -17,12 +17,11 @@ def roman_to_int(roman_string):
             if i == key:
                 list_num.append(r_num[key])
     list_num = list(list_num)
-    m = list_num[0]
-    n = -1
-    for i in list_num:
-        if m >= i:
+    m = 0
+    for i in reversed(list_num):
+        if i >= m:
             num = num + i
         else:
-            num = i - num
-        m = list_num[n + 1]
+            num = num - i
+        m = i
     return num
