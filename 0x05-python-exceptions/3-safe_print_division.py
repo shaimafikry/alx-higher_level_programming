8ab9135e5b77aba030404d02ae91c45c0bb578e6
shaimaa <shaimafikry@yahoo.com>
2024-01-01 19:14:05 +0200

    edit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return result
+    except:
+        return None
+    finally:
+        print("Inside result: {:d}".format(result))
+        
+    
+    
+    
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
