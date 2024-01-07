586f5537be824fcf13f5b9e7413ce8b22f4f646d
shaimaa <shaimafikry@yahoo.com>
2024-01-07 15:57:40 +0200

    edit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -11,4 +11,24 @@ class Square:
     Attributes:
     size: size of square
     """
-    _size
+    def __init__(Self, __size = 0):
+        """initilizations
+
+        Args:
+            Self (size): square size
+        """
+        
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
