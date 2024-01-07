bb2775a8a63874de5347283522d2ff886c690b44
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-01-07 03:42:03 +0200

    Update 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class called Square.
+"""
+
+
+class Square:
+    """
+    This class represents a geometric square.
+
+    Attributes:
+    size: size of square
+    """
+    _size
