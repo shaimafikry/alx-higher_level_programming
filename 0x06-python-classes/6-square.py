16f696761553086d430a6b091428d23cc6d48cae
shaimaa <shaimafikry@yahoo.com>
2024-01-08 18:24:14 +0200

    edit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,7 +80,7 @@ class Square:
         """
         if self.size == 0:
             print()
-        for i in range(self.area()):
+        for i in range(self.__size):
             if self.__position[1] == 0:
                 print("_" * self.__position[0] + "#" * self.__size)
             else:
