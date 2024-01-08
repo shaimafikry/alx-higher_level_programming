51eaff9aeb93f609bcdb1028e0f323e69098bae5
shaimaa <shaimafikry@yahoo.com>
2024-01-08 18:55:49 +0200

    edit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -96,9 +96,10 @@ class Square:
         if self.size == 0:
             print()
             return
+        if self.__position[1] >= 0:
+            print()
         for i in range(self.__size):
             if self.__position[1] == 0:
                 print("_" * self.__position[0] + "#" * self.__size)
             else:
-                print()
                 print(" " * self.__position[0] + "#" * self.__size)
