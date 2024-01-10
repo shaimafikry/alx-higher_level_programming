57773f407cf9498c67e974a1dc89ca2ea3f17186
shaimaa <shaimafikry@yahoo.com>
2024-01-10 07:49:13 +0200

    edit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -88,18 +88,3 @@ class Square:
                 print("_" * self.__position[0] + "#" * self.__size)
             else:
                 print(" " * self.__position[0] + "#" * self.__size)
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
