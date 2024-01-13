5440cf42863b676bbd99ac43b2a6fc5983d98a39
shaimaa <shaimafikry@yahoo.com>
2024-01-13 04:54:43 +0200

    add a new task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+def add_integer(a, b=98):
+    """function to add two integers
+
+    Args:
+        a (int): 
+        b (int, optional):  Defaults to 98.
+    """
+
+    if not isinstance(a,(int, float)):
+        raise TypeError ("a must be an integer")
+    elif not isinstance(b,(int, float)):
+        raise TypeError ("b must be an integer")
+    else:
+        return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,4 @@
+python task for practising :
+documentating
+doctest
+unittest
