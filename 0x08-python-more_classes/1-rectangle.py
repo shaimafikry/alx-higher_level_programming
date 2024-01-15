64fa88e2562a55bf254e840930559d7df8059523
shaimaa <shaimafikry@yahoo.com>
2024-01-15 08:54:35 +0200

    dd new task

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""this module is for rectangle class 
+    """
+ 
+ 
+class Rectangle:
+    """rectangle class to calculate size
+    """
+
+    def __init__(self, width=0, height=0):
+        """initiation 
+        Args:
+            width: int >0
+            height: int > 0
+        """
+        self.__width = width
+        self.__height = height
+        
+    @property
+    def width(self):
+        return self.__width
+    @property
+    def height(self):
+        return self.__height
+    @width.setter
+    def width(self, value):
+        if not isinstance(self.__width,(int)):
+            raise TypeError(
+                "width must be an integer")
+        elif self.__width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    @height.setter
+    def height(self, value):
+        if not isinstance(self.__height,(int)):
+            raise TypeError(
+                "width must be an integer")
+        elif self.__height < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
