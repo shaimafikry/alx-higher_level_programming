e13353037cef7c43d4ed784e4640a779dabbbefd
shaimaa <shaimafikry@yahoo.com>
2024-01-16 22:32:42 +0200

    dd new task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,13 +18,11 @@ def matrix_divided(matrix, div):
     """
 
     if not all(isinstance(k, list) for k in matrix):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     # makes matrix as one list to check every elemnt
     flattened = [item for sublist in matrix for item in sublist]
     if not all(isinstance(item, (int, float)) for item in flattened):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
--- 0x07-python-test_driven_development/tests/3-say_my_name.py
+++ 0x07-python-test_driven_development/tests/3-say_my_name.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """modle for say my name
     """
-
-
 def say_my_name(first_name, last_name=""):
     """prints my name
 
@@ -11,7 +9,7 @@ def say_my_name(first_name, last_name=""):
         last_name (str, optional): sec nmae , defaults to "".
     """
     if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string ")
+        raise TypeError ( "first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string ")
+        raise TypeError ( "last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
