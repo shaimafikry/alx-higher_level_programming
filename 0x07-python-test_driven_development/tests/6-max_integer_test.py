46dda0d65e0b9abb6f1d7466981e768eb04a740f
shaimaa <shaimafikry@yahoo.com>
2024-01-17 07:10:12 +0200

    dd new task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,21 @@ class TestMaxInteger(unittest.TestCase):
         m = max_integer([])
         self.assertEqual(m, None)
 
-    def pass_test(self):
+    def  max_at_the_begining(self):
+        m = max_integer([6, 2, 3])
+        self.assertEqual(m, 6)
+    def max_at_the_end (self):
         m = max_integer([1, 2, 3])
         self.assertEqual(m, 3)
+    def one_negitive_exist(self):
+        m = max_integer([6, -1, 3])
+        self.assertEqual(m, 6)
+    def  max_at_the_middle(self):
+        m = max_integer([6, 9, 3])
+        self.assertEqual(m, 9)
+    def only_negitive_exist(self):
+        m = max_integer([-2, -3, -7])
+        self.assertEqual(m, -2)
+    def list_of_one_elemnt(self):
+        m = max_integer([6])
+        self.assertEqual(m, 6)
