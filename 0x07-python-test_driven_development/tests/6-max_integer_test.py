cbb0aa5ca4750401e4a61f336bfb38a06f74fe4a
shaimaa <shaimafikry@yahoo.com>
2024-01-18 09:07:15 +0200

    dd new task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,31 +8,31 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def nonlist_test(self):
+    def test_nonlist_test(self):
         m = []
         self.assertEqual(max_integer(m), None)
 
-    def max_at_the_begining(self):
+    def test_max_at_the_begining(self):
         m = [6, 2, 3]
         self.assertEqual(max_integer(m), 6)
 
-    def max_at_the_end(self):
+    def test_max_at_the_end(self):
         m = [1, 2, 3]
         self.assertEqual(max_integer(m), 3)
 
-    def one_negitive_exist(self):
+    def test_one_negitive_exist(self):
         m = [6, -1, 3]
         self.assertEqual(max_integer(m), 6)
 
-    def max_at_the_middle(self):
+    def test_max_at_the_middle(self):
         m = [6, 9, 3]
         self.assertEqual(max_integer(m), 9)
 
-    def only_negitive_exist(self):
+    def test_only_negitive_exist(self):
         m = [-2, -3, -7]
         self.assertEqual(max_integer(m), -2)
 
-    def list_of_one_elemnt(self):
+    def test_list_of_one_elemnt(self):
         m = [6]
         self.assertEqual(max_integer(m), 6)
 
