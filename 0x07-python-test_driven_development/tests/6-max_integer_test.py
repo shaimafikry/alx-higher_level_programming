603d7ca9c77a6c03d34756423188df0cc8a4f89f
shaimaa <shaimafikry@yahoo.com>
2024-01-18 08:59:38 +0200

    dd new task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,32 +9,32 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def nonlist_test(self):
-        m = max_integer([])
-        self.assertEqual(m, None)
+        m = []
+        self.assertEqual(max_integer(m), None)
 
     def max_at_the_begining(self):
-        m = max_integer([6, 2, 3])
-        self.assertEqual(m, 6)
+        m = [6, 2, 3]
+        self.assertEqual(max_integer(m), 6)
 
     def max_at_the_end(self):
-        m = max_integer([1, 2, 3])
-        self.assertEqual(m, 3)
+        m = [1, 2, 3]
+        self.assertEqual(max_integer(m), 3)
 
     def one_negitive_exist(self):
-        m = max_integer([6, -1, 3])
-        self.assertEqual(m, 6)
+        m = [6, -1, 3]
+        self.assertEqual(max_integer(m), 6)
 
     def max_at_the_middle(self):
-        m = max_integer([6, 9, 3])
-        self.assertEqual(m, 9)
+        m = [6, 9, 3]
+        self.assertEqual(max_integer(m), 9)
 
     def only_negitive_exist(self):
-        m = max_integer([-2, -3, -7])
-        self.assertEqual(m, -2)
+        m = [-2, -3, -7]
+        self.assertEqual(max_integer(m), -2)
 
     def list_of_one_elemnt(self):
-        m = max_integer([6])
-        self.assertEqual(m, 6)
+        m = [6]
+        self.assertEqual(max_integer(m), 6)
 
 
 if __name__ == "__main__":
