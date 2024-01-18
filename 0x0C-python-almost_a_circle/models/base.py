d445f58f6626e148d97a0bd734b8b4302eab87c6
shaimaa <shaimafikry@yahoo.com>
2024-01-18 09:59:32 +0200

    dd new task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module for the base class 
+    """
+class Base:
+    """the base class
+ 
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """constrcutor
+
+        Args:
+            id (_type_, int): _takes id and assign it to the objecct_. Defaults to None.
+        """
+        if id != None:
+            self.id = id
+        Base.__nb_objects += 1
