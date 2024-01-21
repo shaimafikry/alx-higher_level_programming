c734e2b1e24ad875d58ed0e432e5e429b0334a2c
shaimaa <shaimafikry@yahoo.com>
2024-01-22 00:19:41 +0200

    edit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+"""my list class
+    """
+class MyList (list):
+    """class mylist inherits from list class
+
+    Args:
+        list (_class_): base class
+    """
+    def print_sorted(list):
+        m = sorted(list)
+        print (m)
