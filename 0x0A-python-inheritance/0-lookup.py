c749c9f0d5de444e698ffedca185f86677c49f7d
shaimaa <shaimafikry@yahoo.com>
2024-01-21 23:17:00 +0200

    edit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+module for look up
+"""
+
+
+def lookup(obj):
+    """to look for atrrib and meth of an object
+
+    Args:
+        obj (any time)
+    """
+
+    n_list = dir(obj)
+    return n_list
