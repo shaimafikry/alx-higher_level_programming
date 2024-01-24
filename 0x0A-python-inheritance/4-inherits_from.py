d3626c9632a11cbf0fa85e61a69d5d3f369224b5
shaimaa <shaimafikry@yahoo.com>
2024-01-24 13:52:36 +0200

    edit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,6 +11,10 @@ def inherits_from(obj, a_class):
         a_class (_type_): class check
     """
 
-    if isinstance(obj, a_class):
-        return True
+    if type(obj) is a_class:
+        return False
+
+    for cls in type(obj).mro():
+        if cls is a_class:
+            return True
     return False
