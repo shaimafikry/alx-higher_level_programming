ef21d9765488f1c3d1f97853e6a87edc6bd7161e
shaimaa <shaimafikry@yahoo.com>
2024-01-24 19:39:58 +0200

    add new task

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """module for  Python - Input/Output
     """
-
-
 import json
+
+
 def from_json_string(my_str):
     """convert from json to string
 
     Args:
         my_str (string):
     """
-    return json.load(my_str)
+    return json.loads(my_str)
