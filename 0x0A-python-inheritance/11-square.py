20bdbf726e99d810dedeb068c041b1f8f536af5a
shaimaa <shaimafikry@yahoo.com>
2024-01-24 13:20:38 +0200

    edit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""square class iherts from geometry
+
+    """
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """class square
+    Args:
+        rectangle (base class): base class
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
