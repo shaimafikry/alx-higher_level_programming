9b2e53646e6897b2d21d10673b5352139316aa34
shaimaa <shaimafikry@yahoo.com>
2024-01-24 11:09:19 +0200

    edit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,6 +11,6 @@ def is_same_class(obj, a_class):
         a_class (_type_): class check
     """
 
-    if obj is a_class or isinstance(obj, a_class):
+    if obj is a_class:
         return True
     return False
