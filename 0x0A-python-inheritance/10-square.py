ddd9e855ae52812d0dec4b3d4669f84680fd5054
shaimaa <shaimafikry@yahoo.com>
2024-01-24 13:55:26 +0200

    edit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""
-This module defines a Square class that inherits from the Rectangle class.
-The Square class represents a square shape with a given size.
-It overrides the area method from the Rectangle class to calculate the area of the square.
-    """
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
