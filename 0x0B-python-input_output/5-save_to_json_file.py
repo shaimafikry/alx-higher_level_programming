26b044082c4f2742f70503cd2d177583cf905a56
shaimaa <shaimafikry@yahoo.com>
2024-01-24 20:42:09 +0200

    add new task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
         my_obj (string): to be converted
         filename: to put into
     """
-    with open(filename, "a", encoding="utf-8") as f_new:
+    with open(filename, "w", encoding="utf-8") as f_new:
         json.dump(my_obj, f_new)
