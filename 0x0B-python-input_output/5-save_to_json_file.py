d00bbd65bf30eaa76c3e6c942e197d403ea3ab77
shaimaa <shaimafikry@yahoo.com>
2024-01-24 19:58:55 +0200

    add new task

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for  Python - Input/Output
+    """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """convert from json to string
+
+    Args:
+        my_obj (string): to be converted
+        filename: to put into
+    """
+    with open(filename, "w", encoding="utf-8") as f_new:
+        json.dump(my_obj, f_new)
