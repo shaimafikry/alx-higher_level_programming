e5498bb9bb1b21c384fb0d223412fa22deab7494
shaimaa <shaimafikry@yahoo.com>
2024-01-24 20:36:45 +0200

    add new task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module for  Python - Input/Output
+    """
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+save_to_json_file(sys.argv, "add_item.json")
+my_list = load_from_json_file("add_item.json")
