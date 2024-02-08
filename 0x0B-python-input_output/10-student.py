d4545ec5f04b8fffe0d08421412d8f2c5e9e3c1e
shaimaa <shaimafikry@yahoo.com>
2024-02-08 19:26:23 +0200

    edit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module for  class student
+    """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs:
+            dic = {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            dic = {k: v for k, v in self.__dict__.items()}
+        return dic
