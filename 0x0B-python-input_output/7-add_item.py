fad39bcbb45dc3de424a5f6ed67406ad8aaace52
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-02-08 16:59:53 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """module for  Python - Input/Output
     """
+
+
 import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
+my_list = []
 save_to_json_file(sys.argv[1:], "add_item.json")
-my_list.append(load_from_json_file("add_item.json"))
+item = load_from_json_file("add_item.json")
+my_list.append(item)
+save_to_json_file(my_list, "add_item.json")
