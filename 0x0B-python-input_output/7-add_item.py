5080cfe6d730da31d6045d6e3d1b759666ae7f50
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-02-08 16:53:06 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,4 +8,4 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 save_to_json_file(sys.argv[1:], "add_item.json")
-my_list = load_from_json_file("add_item.json")
+my_list.append(load_from_json_file("add_item.json"))
