51d5ef11d4617d409281ac9ff57a1c74c03982e0
shaimaa <shaimafikry@yahoo.com>
2024-02-09 03:02:47 +0200

    edit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""rectangle inherts from base
+     """
+
+from base import Base
+
+
+class Rectangle(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        Base.__init__(self, id=None)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+
+@property
+def width(self):
+    return self.__width
+
+
+@width.setter
+def width(self, value):
+    self.__width = value
+
+
+@property
+def width(self):
+    return self.__width
+
+
+@width.setter
+def width(self, value):
+    self.__width = value
+
+
+@property
+def height(self):
+    return self.__height
+
+
+@height.setter
+def height(self, value):
+    self.__height = value
+
+
+@property
+def y(self):
+    return self.__y
+
+
+@y.setter
+def y(self, value):
+    self.__y = value
+
+
+@property
+def x(self):
+    return self.__x
+
+
+@x.setter
+def width(self, value):
+    self.__x = value
