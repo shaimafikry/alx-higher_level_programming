d178778b49720faf72132ec91e4a47baaa324198
shaimaa <shaimafikry@yahoo.com>
2024-02-21 21:23:30 +0200

    edit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- script that lists the number of records with the same score in the "second_table"
 -- result should display the score m the number of this score with the lable name
-SELECT score, count(*) AS number FROM second_table 
-WHERE score=score
-GRUOP BY score
+SELECT score, COUNT(*) AS number
+FROM second_table 
+GROUP BY score
 ORDER BY score DESC;
