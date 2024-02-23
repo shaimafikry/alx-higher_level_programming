7fca7fa9df70d6cfd3d37ac11ae64b51d9d7aea7
shaimaa <shaimafikry@yahoo.com>
2024-02-23 23:12:45 +0200

    edit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- script that creates the MySQL server user user_0d_1.
 
-CREATE USER IF NOT EXISTS "user_0d_1@localhost" IDENTITIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS "user_0d_1" IDENTITIFIED BY "user_0d_1_pwd";
 GRANT ALL PRIVILAGES
 ON *.*
 TO user_0d_1;
