c8b67a63b5da446528a3b08554d369070a0704dd
shaimaa <shaimafikry@yahoo.com>
2024-02-24 17:36:10 +0200

    edit

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,4 @@ SELECT  tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 RIGHT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_shows.id
-ORDER BY tv_shows.title , tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
