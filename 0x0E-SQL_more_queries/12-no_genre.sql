e5a0a9267663108296c4b02fcd2003d463852907
shaimaa <shaimafikry@yahoo.com>
2024-02-25 00:46:17 +0200

    edit

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,7 @@
 -- using join
 SELECT  tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-FULL JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id 
-WHERE tv_show_genres.show_id IS NULL
-OR tv_shows.id IS NULL;
+WHERE tv_show_genres.genre__id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
