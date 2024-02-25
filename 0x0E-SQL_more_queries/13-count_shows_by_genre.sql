7317889df234e3190b1c372ab736decb6203929f
shaimaa <shaimafikry@yahoo.com>
2024-02-25 14:42:00 +0200

    edit file 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,5 +5,5 @@ FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-HAVING number_of_shows >  0
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
