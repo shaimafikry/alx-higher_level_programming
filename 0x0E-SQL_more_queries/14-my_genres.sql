46f9953b71d4e9cb34d89d60d18cf2e209b5c4eb
shaimaa <shaimafikry@yahoo.com>
2024-02-25 12:30:53 +0200

    edit file 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN  tv_shows ON tv_show_genres.genre_id = tv_shows.id
+RIGHT JOIN  tv_shows ON tv_show_genres.genre_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
