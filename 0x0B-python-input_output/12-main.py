2d55d90785512badd47b94de3135f9c677fb52b9
shaimaa <shaimafikry@yahoo.com>
2024-03-06 09:27:59 +0200

    edit file 12

--- 0x0B-python-input_output/12-main.py
+++ 0x0B-python-input_output/12-main.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-12-main
-"""
 pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,5 +3,23 @@
     """
  
 def pascal_triangle(n):
-     if n <= 0:
-         return []
+    if n <= 0:
+        return []
+    m_list = [[1], [1,1]]
+    # print ( "this before loop", m_list)
+    new_list = []
+    while n > 0:
+        loop_list = m_list[-1]
+        # print ("this before loop after assignng the last list ", loop_list)
+        new_list = []
+        for i in range(len(loop_list)):
+            # print ("this is i", i)
+            if i != len(loop_list) - 1:
+                new_list.append(loop_list[i] + loop_list[i+1])
+            # print ( "this after elemnt ", new_list)
+        new_list = [1] + new_list + [1]
+        # print ( "this after adding 1 loop", new_list)
+        m_list.append(new_list)
+        # print ( "this after each  loop", m_list)
+        n -= 1
+    return m_list
