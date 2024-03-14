230bb418b6a02a58e78d2c7aa145a4b370010b93
shaimaa <shaimafikry@yahoo.com>
2024-03-14 07:00:10 +0200

    add file 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-const{ argv} = require('node:process');
-if (argv.length <= 2)
-{
-console.log('No argument')
-}
-else
-{
-console.log('Argument found')
-};
+const { argv } = require('node:process');
+if (argv.length <= 2) { console.log('No argument'); } else { console.log('Argument found'); }
