3f919e8b8e1ff800c779506de57a4b7634f822f7
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-03-15 14:24:38 +0200

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction){
+exports.callMeMoby = function(x, theFunction) {
 let i = 0;
-while (i < x) {
+while (i < x) { 
   theFunction();
-  i++;
-}
+  i++; }
 };
