063e8c0d8de85da1c64e55cfbdae20460517bff1
shaimaa <shaimafikry@yahoo.com>
2024-03-19 12:03:14 +0200

    add file 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     u_pass = argv[2]
     db_name = argv[3]
     state_name = argv[4]
-    port = "3306"
+    port = 3306
     host = "localhost"
-    db_access = MySQLdb.connect(host, u_name, u_pass, db_name, charset="utf8")
+    db_access = MySQLdb.connect(host, u_name, u_pass, db_name, port)
     sql_order = db_access.cursor()
     sql_order.execute(
         "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC;", (state_name,))
