c1b7b95a9ab258e1b5c25259b6547876b2b66518
shaimaa <shaimafikry@yahoo.com>
2024-03-19 15:20:02 +0200

    add file 2

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,6 +7,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    n_list = []
     u_name = argv[1]
     u_pass = argv[2]
     db_name = argv[3]
@@ -15,10 +16,9 @@ if __name__ == "__main__":
     db_access = MySQLdb.connect(host, u_name, u_pass, db_name, port)
     sql_order = db_access.cursor()
     sql_order.execute(
-        "SELECT name FROM cities WHERE state_id = (SELECT id from states WHERE name = %s) ORDER BY cities.id ASC;",
+        "SELECT name FROM cities WHERE state_id = (SELECT id from states WHERE name = %s)ORDER BY cities.id ASC;",
         (argv[4],),
     )
-    n_list = []
     query_rows = sql_order.fetchall()
     for row in query_rows:
         n_list.append(row[0])
