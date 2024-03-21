9055b7774b4b73a21448ffff0672a1e7e30007a6
shaimaa <shaimafikry@yahoo.com>
2024-03-21 13:41:38 +0200

    edit

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ this module connect the database with python using argv
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ class to create the first table"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
