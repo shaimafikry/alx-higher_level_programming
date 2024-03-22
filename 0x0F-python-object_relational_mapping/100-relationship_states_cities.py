8736a4c83ff8dbc753b2777601c6aad9945e8f66
shaimaa <shaimafikry@yahoo.com>
2024-03-22 15:17:51 +0200

    edit

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,11 +3,11 @@
 """
 
 import sys
-from model_state import Base, State
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
-from model_city import City, Base
 
 
 if __name__ == "__main__":
@@ -17,25 +17,11 @@ if __name__ == "__main__":
         ),
         pool_pre_ping=True,
     )
+    # creates all tables included, or obtain it from database
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
     session = Session(engine)
-    cities = session.query(City, State).filter(City.state_id == State.id)
-    for city, state in cities:
-        print(("{}: ({}) {}").format(state.name, city.id, city.name))
+    new_state = session.add(State(name='California'))
+    new_city = session.add(City(name='San Francisco'), state=new_state)
+    session.commit()
     session.close()
-
-
-
-
-
-State class:
-In addition to previous requirements, the class attribute cities must represent a relationship with the class City. If the State object is deleted, all linked City objects must be automatically deleted. Also, the reference from a City object to his State should be named state
-
-Write a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)
-
-Your script should take 3 arguments: mysql username, mysql password and database name
-You must use the module SQLAlchemy
-Your script should connect to a MySQL server running on localhost at port 3306
-You must use the cities relationship for all State objects
-Your code should not be executed when imported
