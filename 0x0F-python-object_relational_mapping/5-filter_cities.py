ff0576c6fa29ca6e0d5860dfb683aacc8fa991fa
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-03-24 17:14:01 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -22,8 +22,12 @@ if __name__ == "__main__":
     )
     sql_order.execute(sql_text, (argv[4],))
     query_rows = sql_order.fetchall()
-    for row in query_rows:
-        n_list.append(row[0])
-    print(", ".join(n_list))
+    string = ''
+    for i in range(0,len(query_rows)):
+        string += query_rows[i][0]
+        if i == len(query_rows) - 1:
+            break
+        string += ', '
+    print(string)
     sql_order.close()
     db_access.close()
