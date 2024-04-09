d616a2eb708836e1067280d2cc074774d15389f3
shaimaa <shaimafikry@yahoo.com>
2024-04-09 04:44:53 +0200

    add 102

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# import dis
 def magic_calculation(a, b):
-    """ """
-    
+    """represent the function in bytecode """
+    return (a ** b) + 98
+# b = dis.dis(magic_calculation)
+# print(b)
