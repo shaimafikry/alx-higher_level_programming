b79b0761e90a9e496265f2a22c065d3d1431092a
shaimaa <shaimafikry@yahoo.com>
2024-04-09 04:47:07 +0200

    add 102

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# how to Disassembler for Python bytecode
 # import dis
 def magic_calculation(a, b):
-    """represent the function in bytecode """
+    """normal function """
     return (a ** b) + 98
+# represent the function in bytecode
 # b = dis.dis(magic_calculation)
 # print(b)
