9b8c2b73fd9fb8d679701e08a275f39b57bd00b9
shaimaa <shaimafikry@yahoo.com>
2024-04-12 07:54:04 +0200

    add 101 sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
+// script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
 const dict = require('./101-data').dict;
 
-let newDict = {};
-// to be continue
-console.log(dict);
-console.log(newDict);
+const numOfId = {};
+let key, value;
+for ([key, value] of Object.entries(dict)) {
+  const strValue = value.toString();
+  if (numOfId[strValue]) {
+    numOfId[strValue].push(key);
+  } else {
+    numOfId[strValue] = [key];
+  }
+}
+
+console.log(numOfId);
