e859f2d2bbe3f9f08dde4972684d2cb75f407d86
shaimaa <shaimafikry@yahoo.com>
2024-04-12 23:32:36 +0200

    edit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,18 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const file1 = argv[2];
-const file2 = argv[3];
-const dest = argv[4];
+const fs = require('fs');
+// to obtain the path to the file you want to read from argument
+const { argv } = require('process');
 
-// to be continue
-console.log(argv);
+// define variables to hold data
+let data1, data2, content;
+try {
+ // Use fs.readFileSync to read the file
+  data1 = fs.readFileSync(argv[2], 'utf8');
+  data2 = fs.readFileSync(argv[3], 'utf8');
+ // add them together
+ content = data1 + data2;
+} catch (err) {
+ console.error('An error occurred:', err);
+}
+// to write toa file using flags
+fs.writeFile(argv[4], content,  { flag: 'w+' }, err => {});
