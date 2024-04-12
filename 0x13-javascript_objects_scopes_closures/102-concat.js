71d787e2ab65e269cd900bb681db82a89f9b2c1a
shaimaa <shaimafikry@yahoo.com>
2024-04-12 23:39:58 +0200

    edit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,13 +6,13 @@ const { argv } = require('process');
 // define variables to hold data
 let data1, data2, content;
 try {
- // Use fs.readFileSync to read the file
+  // Use fs.readFileSync to read the file
   data1 = fs.readFileSync(argv[2], 'utf8');
   data2 = fs.readFileSync(argv[3], 'utf8');
- // add them together
- content = data1 + data2;
+  // add them together
+  content = data1 + data2;
 } catch (err) {
- console.error('An error occurred:', err);
+  console.error('An error occurred:', err);
 }
 // to write toa file using flags
-fs.writeFile(argv[4], content,  { flag: 'w+' }, err => {});
+fs.writeFile(argv[4], content, { flag: 'w+' }, err => { console.error('An error occurred:', err); });
