c54e3190503fff819adcf75470fdde07168d02c2
shaimaa <shaimafikry@yahoo.com>
2024-05-01 16:08:15 +0300

    edit

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST -d "$2" -H "Content-Type: application/json" "$1"
+curl -sX POST -H "Content-Type: application/json" -d "$2" "$1"
