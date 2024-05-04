e1b6d5b0f1368141cd5652498ed590cb0288cc53
shaimaa <shaimafikry@yahoo.com>
2024-05-04 19:51:00 +0300

    add the first file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,7 @@
 
 if __name__ == "__main__":
     import urllib.request
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: <class 'bytes'>")
