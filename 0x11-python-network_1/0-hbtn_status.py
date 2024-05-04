fb3b994b5f67037584e2045b9677f53fc40b8396
shaimaa <shaimafikry@yahoo.com>
2024-05-04 20:02:43 +0300

    add the first file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         html = response.read()
         print("Body response:")
-        print("\t- type:<class 'bytes'>")
+        print("\t- type: <class 'bytes'>")
         print("\t- content:", html)
         print("\t- utf8 content:", html.decode('utf-8'))
