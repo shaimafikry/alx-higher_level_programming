f6f78cf1b9983513250103703646352af9479a05
shaimaa <shaimafikry@yahoo.com>
2024-06-02 00:31:51 +0300

    add 101th task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,20 +5,19 @@ const request = require('request');
 const { argv } = require('node:process');
 // The first argument is the movie ID
 const filmId = argv[2];
-
 const url = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
 // using function to catch the error if occured and body and response
 request.get(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const movies = JSON.parse(body);
-	let i = 0;
-    for (i = 0; i < movies.characters.length; i++) {
-      request.get(movies.characters[i], function (error, response, body) {
+    const movie = JSON.parse(body);
+    const movieChars = movie.characters;
+    for (const char of movieChars) {
+      request.get(char, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-          const chart = JSON.parse(body);
-          console.log(chart.name);
+          const charNames = JSON.parse(body);
+          console.log(charNames.name);
         }
       });
-    };
+    }
   }
 });
